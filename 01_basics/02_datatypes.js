a3f6d70bda9d22a03045fb9d0b9ddaac495d5c78
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-10 07:22:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser so that is why this will not give 6 as the answer
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("hiii");
+
+
+let name = "kriti" // string datatype
+let age = 22  // number datatype
+let isLoggedIn = false  //boolean datatype
+
+
+/* types of datatypes:
+
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned yet
+symbol => unique
+
+
+object
+
+*/
+
+console.log(typeof null);  //object
+
+console.log(typeof undefined); //undefined
