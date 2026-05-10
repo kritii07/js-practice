ca8fb2ee18398555c1bf1f616037ab655f1e84e8
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-10 09:36:14 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion operation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber); //NaN becoz it is 33abc
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kriti" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
