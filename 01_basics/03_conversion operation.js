7f34a3f0e2acd77680eebd3aa27c2e51a20a8f8f
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-10 09:57:48 +0000

    operations

--- 01_basics/03_conversion operation.js
+++ 01_basics/03_conversion operation.js
@@ -20,7 +20,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -30,5 +30,50 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+
+
+// ****************OPERATIONS****************
+
+let value = 3
+let negVlue = -value
+//console.log(negVlue);
+
+/*
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); //power
+console.log(2/2);
+console.log(2%2);
+
+*/
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// messy codes (DO NOT USE THEM)
+console.log("1" + 2); // => 12
+console.log(1 + "2"); // => 12
+console.log("1" + 2 + 2); // => 122
+console.log(1 + 2 + "2"); // => 32
+
+console.log(+true); // => 1
+//console.log(true+); // => error
+console.log(+""); // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;  //++gameCounter  => both give 101 
+//read syntax of both through mdn
+console.log(gameCounter);
