ba9016b5f86ea9963f44a2d9ac26e6b72ef4cd2f
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-10 11:02:30 +0000

    stack and heap concept

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -25,3 +25,38 @@ const myFunction = function(){
     console.log("hello world");
 }
 
+// *******************************************************************************
+
+
+// stack (primitve), heap(non-primitive)
+
+let name1 = "raj"
+
+let name2 = name1
+name2 = "kiran"
+
+console.log(name1); // raj
+console.log(name2); // kiran
+
+// name2 can not change name one becoz name 2 does not have the original copy of name1, it has a duplicate copy , so that is why it can not change name1
+
+
+//STACK me copy milta hai or HEAP me reference milta hai(i.e. changes original value ke andr hi hote hai)
+
+
+//non primitive datatypes use HEAP
+ let userOne = {
+    email: "abc@gmail.com",
+    upi: "user@ybl"
+ }
+
+ let userTwo = userOne
+
+ userTwo.email = "pqr@gmail.com"
+
+ console.log(userOne.email); //pqr@gmail.com
+ console.log(userTwo.email); //pqr@gmail.com
+
+ // here userTwo has changed the value of userOne becos it has the reference to the original copy, so the changes will refelect in the original copy as it uses HEAP
+ 
+
