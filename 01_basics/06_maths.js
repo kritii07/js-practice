2d3e8917ee43abc0b5ded1e8c96c8fc95d97763e
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-12 06:55:27 +0000

    maths concept done

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -20,3 +20,27 @@ const hundreds = 10000000
 
 
 //++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));  //negative to positive
+
+console.log(Math.round(4.6))  //round off value
+
+console.log(Math.ceil(4.2));  //5
+
+console.log(Math.floor(4.8))  //4
+
+console.log(Math.min(3,2,5,4,65,64)); //gives minimum value
+console.log(Math.max(3,2,5,4,65,64)); //gives max value
+
+console.log(Math.random());  //value between 0-1
+
+console.log(Math.floor(Math.random()*10) +1);
+
+
+//NOTE:
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  //IMPORTANT FORMULA
