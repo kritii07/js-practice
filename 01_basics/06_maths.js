f538721e9b64b9e725409a9d77dd7a64c99f6e87
Kriti <127346997+kritii07@users.noreply.github.com>
2026-05-12 06:41:25 +0000

    numbers covered

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,22 @@
+const score = 400
+//console.log(score);  //=> 400
+
+const balance = new Number(100)
+//console.log(balance);  //=> [Number: 100]
+//console.log(balance.toString());  //=> 100
+//console.log(balance.toString().length);  //=> 3
+//console.log(balance.toFixed(2));  //100.00
+
+const otherNumber = 23.898
+//console.log(otherNumber.toPrecision(3));  //=>23.9
+
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString());  //=>
+//console.log(hundreds.toLocaleString());  //=>10,000,000
+
+//console.log(hundreds.toLocaleString('en-IN'));  //=> 1,00,00,000  put commas according to indian standards
+
+
+
+//++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++
